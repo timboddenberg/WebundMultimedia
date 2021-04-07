8fa16db97f6ecac9931aa8ab885abf68a7b84a7e
timboddenberg <timbodd1996@gmail.com>
2021-04-07 17:14:36 +0200

    add index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php
+<html lang="de">
+
+    <head>
+
+        <meta charset="utf-8">
+        <link href="Css/Bootstrap/bootstrap.min.css" rel="stylesheet"/>
+        <link href="Css/Bootstrap/bootstrap-theme.min.css" rel="stylesheet"/>
+
+        <title>
+            Mike der Lachs
+        </title>
+    </head>
+
+    <body>
+        <script src="Js/JQuery/JQuery.min.js"></script>
+        <script src="Js/Bootstrap/bootstrap.min.js"></script>
+    </body>
+
+</html>
